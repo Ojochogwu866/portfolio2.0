e478c19e5816a9f34e7b90fe52889aeca96831f5
Ojochogwu <justyouandickson@gmail.com>
2023-01-12 00:20:54 +0100

    resume-update

--- src/views/Pages/Resume/Resume.vue
+++ src/views/Pages/Resume/Resume.vue
@@ -1,98 +1,156 @@
 <template>
-    <div class="w-full text-left m-auto h-full p-12 sx:p-4 flex flex-col justify-center items-center bg-gray-100">
-        <div class=" w-full flex flex-col justify-end items-end">
-            <a href="https://www.linkedin.com/in/ojochogwu-d-0838a312b/" target="_blank" class=""><img class="mr-2 w-4" src="@/assets/Images/linkedin(1).png"/></a>
-            
-            <!-- <a  class=" flex justify-center items-center"><img class="mr-2 w-3 h-3" src="@/assets/Images/download-circular-button.png"/>Download</a> -->
-        </div>
-        <div class="" >
-            <div class="w-full  flex justify-center items-start flex-col">
-                <div class=" text-7xl sx:text-5xl text-work font-bold tracking-wide">Ojochogwu<br/> Dickson</div>
-                <div class=" text-lg font-light">Front-End Developer & Ux Engineer</div>
-                <div class=" flex justify-start mt-4 items-start sx:flex-col">
-                    
-                     <a href="mailto:<nowiki>dicksonojochogwu@gmail.com?subject=HTML link" class="flex justify-center items-center text-sm font-light tracking-wider"><img class=" mr-2 w-4 h-4 " src="@/assets/Images/mail.png"/>dicksonojochogwu@gmail.com</a>
-                    <div class=" ml-4  sx:ml-0 text-sm flex justify-center items-center font-light tracking-wider"><img class="mr-2 w-4 h-4" @click="call" src="@/assets/Images/phone-call.png"/>234 70 547 00832</div>
-                    <a href="https://ojochogwu.xyz" target="_blank" class="text-sm  ml-4 sx:ml-0 flex justify-center items-center font-light tracking-wider"><img class="mr-2 w-4 h-4" src="@/assets/Images/home(1).png"/>ojochogwu.xyz</a>
-                    <a href="https://github.com/Ojochogwu866" target="_blank" class="text-sm ml-4 sx:ml-0 flex justify-center items-center font-light tracking-wider"><img class="mr-2 w-4 h-4" src="@/assets/Images/github(1).png"/>github.com/Ojochogwu866</a>
-                </div>
-         
-            </div>
-            <div class="w-full  mt-4 flex sx:flex-col">
-                <div class="flex flex-col w-3/5 sx:w-11/12 sx:pr-0 pr-4">
-                    <div class=" text-lg text-header font-bold">Relevant Experience</div>
-                    <div v-for="(company, index) in data.companies"
-                    :key="index" class="flex flex-col mt-4">
-                        <div class=" text-sm font-medium text-work">{{company.role}} <span class=" text-gray-600 ">{{company.name}} — {{company.term}}</span></div>
-                        <span class=" mt-1 text-xs font-light tracking-wide">{{company.period}}</span>
-                        <div class="mt-4">
-                            <div class=" text-sm">{{company.intro}}</div>
-                        <ul>
-                            <li v-for="(point, index) in company.points" :key="index" class="leading-7 text-sm   ">{{point}}</li>
-                         </ul>                  
-                        </div>
-                    </div>
-               
-                </div>
-                <div class=" w-2/5 sx:w-full">
-                    <div class="">
-                        <div class=" text-header font-bold text-lg">Core Technologies:</div>
-                        <div class=" text-work text-sm font-bold mt-4">Programming Languages</div>
-                        <div v-for="(tech, index) in data.technologies" :key="index" class=" text-sm leading-6 font-light tracking-wide">{{tech}}
-                        </div>
-                    </div>
-                       <div class=" mt-4">
-                        <div class=" text-work text-sm font-bold">Libraries & Frameworks</div>
-                        <div v-for="(library, index) in data.libraries" :key="index" class=" text-sm font-light tracking-wide leading-6">{{library}}</div>
-                    </div>
-                      <div class=" mt-4">
-                         <div class=" text-work text-sm font-bold">Design</div>
-                        <div v-for="(des, index) in data.design" :key="index" class=" text-sm font-light tracking-wide leading-6">{{des}}</div>
-                    </div>
-                      <div class=" mt-2">
-                         <div class=" text-work text-sm font-bold">Others</div>
-                        <div v-for="(others, index) in data.others" :key="index" class=" text-sm font-light tracking-wide leading-6">{{others}}</div> 
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div
+    class="w-full text-left m-auto h-full p-12 sx:p-4 flex flex-col justify-center items-center bg-gray-100"
+  >
+    <div class="w-full flex flex-col justify-end items-end">
+      <a
+        href="https://www.linkedin.com/in/ojochogwu-d-0838a312b/"
+        target="_blank"
+        class=""
+        ><img class="mr-2 w-4" src="@/assets/Images/linkedin(1).png"
+      /></a>
+
+      <!-- <a  class=" flex justify-center items-center"><img class="mr-2 w-3 h-3" src="@/assets/Images/download-circular-button.png"/>Download</a> -->
     </div>
+    <div class="">
+      <div class="w-full flex justify-center items-start flex-col">
+        <div class="text-7xl sx:text-5xl text-work font-bold tracking-wide">
+          Ojochogwu Dickson
+        </div>
+        <div class="text-lg font-light">Front-End Developer & Ux Engineer</div>
+        <div class="flex justify-start mt-4 items-start sx:flex-col">
+          <a
+            href="mailto:<nowiki>dicksonojochogwu@gmail.com?subject=HTML link"
+            class="flex justify-center items-center text-sm font-light tracking-wider"
+            ><img
+              class="mr-2 w-4 h-4"
+              src="@/assets/Images/mail.png"
+            />dicksonojochogwu@gmail.com</a
+          >
+          <div
+            class="ml-4 sx:ml-0 text-sm flex justify-center items-center font-light tracking-wider"
+          >
+            <img
+              class="mr-2 w-4 h-4"
+              @click="call"
+              src="@/assets/Images/phone-call.png"
+            />234 70 547 00832
+          </div>
+          <a
+            href="https://ojochogwu.xyz"
+            target="_blank"
+            class="text-sm ml-4 sx:ml-0 flex justify-center items-center font-light tracking-wider"
+            ><img
+              class="mr-2 w-4 h-4"
+              src="@/assets/Images/home(1).png"
+            />ojochogwu.xyz</a
+          >
+          <a
+            href="https://github.com/Ojochogwu866"
+            target="_blank"
+            class="text-sm ml-4 sx:ml-0 flex justify-center items-center font-light tracking-wider"
+            ><img
+              class="mr-2 w-4 h-4"
+              src="@/assets/Images/github(1).png"
+            />github.com/Ojochogwu866</a
+          >
+        </div>
+      </div>
+      <div class="w-full mt-4 flex sx:flex-col">
+        <div class="flex flex-col w-3/5 sx:w-11/12 sx:pr-0 pr-4">
+          <div class="text-lg text-header font-bold">Relevant Experience</div>
+          <div
+            v-for="(company, index) in data.companies"
+            :key="index"
+            class="flex flex-col mt-4"
+          >
+            <div class="text-sm font-medium text-work">
+              {{ company.role }}
+              <span class="text-gray-600"
+                >{{ company.name }} — {{ company.term }}</span
+              >
+            </div>
+            <span class="mt-1 text-xs font-light tracking-wide">{{
+              company.period
+            }}</span>
+            <div class="mt-4">
+              <ul>
+                <li
+                  v-for="(point, index) in company.points"
+                  :key="index"
+                  class="leading-7 text-sm"
+                >
+                  {{ point }}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="w-2/5 sx:w-full">
+          <div class="">
+            <div class="text-header font-bold text-lg">Core Technologies:</div>
+            <div class="text-work text-sm font-bold mt-4">
+              Programming Languages
+            </div>
+            <div
+              v-for="(tech, index) in data.technologies"
+              :key="index"
+              class="text-sm leading-6 font-light tracking-wide"
+            >
+              {{ tech }}
+            </div>
+          </div>
+          <div class="mt-2">
+            <div class="text-work text-sm font-bold">Others</div>
+            <div
+              v-for="(others, index) in data.others"
+              :key="index"
+              class="text-sm font-light tracking-wide leading-6"
+            >
+              {{ others }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
-ul li{
-    list-style: square;
+ul li {
+  list-style: square;
 }
 </style>
 <script>
-import data from './data'
-import jsPDF from 'jspdf'
-import axios from 'axios'
+import data from "./data";
+import jsPDF from "jspdf";
+import axios from "axios";
 export default {
-    components:{data, jsPDF},
-    setup() { 
+  components: { data, jsPDF },
+  setup() {},
+  data() {
+    return {
+      data,
+      pdfLink: require("@/assets/Images/ojochogwu-resume.pdf"),
+    };
+  },
+  methods: {
+    call() {
+      window.open("tel:+2347054700832");
     },
-    data(){
-        return{
-            data,
-            pdfLink:require("@/assets/Images/ojochogwu-resume.pdf"),
-       }
+    downloadFile() {
+      axios({
+        url: "http://localhost:8800/ojochogwu-resume.pdf",
+        method: "GET",
+        responseType: "blob",
+      }).then((res) => {
+        var FILE = window.URL.createObjectURL(new Blob([res.data]));
+        var docUrl = document.createElement("x");
+        docUrl.href = FILE;
+        docUrl.setAttribute("download", "file.pdf");
+        document.body.appendChild(docUrl);
+        docUrl.click();
+      });
     },
-    methods:{
-            call () {
-      window.open('tel:+2347054700832')},
-      downloadFile() {
-              axios({
-                    url: 'http://localhost:8800/ojochogwu-resume.pdf',
-                    method: 'GET',
-                    responseType: 'blob',
-                }).then((res) => {
-                     var FILE = window.URL.createObjectURL(new Blob([res.data])); 
-                     var docUrl = document.createElement('x');
-                     docUrl.href = FILE;
-                     docUrl.setAttribute('download', 'file.pdf');
-                     document.body.appendChild(docUrl);
-                     docUrl.click();
-                });
-},}
-}
+  },
+};
 </script>
--- src/views/Pages/Resume/data.js
+++ src/views/Pages/Resume/data.js
@@ -1,72 +1,56 @@
 export default {
-    companies: [
-      {
-        name: '@Clinikli',
-        period: 'Nov 2021 - August 2022 (Remote)',
-        role: 'UI Engineer',
-        term:'Part-Time Role',
-        intro:
-        'Involved in the development of a user interface that would be handling appointment booking for doctors, patients, hospitals and multiple clients around Africa. ',
-        brief: `Independently worked on putting up the Frontend Infrastructure, implementing pixel perfect designs and Ux friendly interfaces`,
-        points: [
-          'Write modern, performant, and robust code for a diverse array of client and internal projects',
-          'Worked with a variety of different languages, frameworks, and content management systems such as JavaScript, TypeScript,  Vue,  Node.js',
-          'Communicate and collaborate with multi-disciplinary teams of engineers, designers and clients.',
-        ],
-      },
-      {
-        name: '@Sonvisage',
-        period: 'November 2021 - March 2022 (Remote)',
-        role: 'UI Engineer',
-        term:'Part-Time Role',
-        intro:
-          'Principal Frontend Enigineer, Translating designs to full blown web-app intended for managing, and engaging data of millions of Kids and Adults in Africa.',
-        points: [
-          'Wrote modern and optimized code to serve a large client base which would engage concurrently',
-          "Built the Frontend Infrastructure for major component of the web app, built in VueJs and Javascript(vanilla).",
-          'Implemented fast and reliable data transformation and diffing mechanisms on the frontend for our data pipelines and Delivered an overall fast, yet accessible and silky smooth user experience in the app.',
-        ],
-      },
-      {
-        name: '@Vinca',
-        period: 'Sept 2021 - November 2021 (Remote)',
-        role: 'UI Engineer',
-        term:'Part-Time Role',
-        intro: `Built the entire Ui of the web-app, which would be handling appointment booking and clients management, alongside communication`,
-        points: [
-          'Built the Frontend application of the Web app from ground up',
-          'Rebuilt the entire web app original written in HTML&CSS using React',
-          'Interacted alot with the clients and other users to implement necessary features as approved by the stakeholders for effective running of the interface.',
-          'Built an appointment scheduling system from the ground up with React.',
-          'Delivered a fast, clean and scalable Ui, optimized for fast performance alongside the Admin component.',
-        ],
-      },
-      {
-        name: '@FourthStream',
-        period: 'December 2020 - April 2021 (Remote)',
-        role: 'Principal Frontend Engineer',
-        term:" Contract",
-        intro:
-          'Worked as the principal frontend engineer for fourthstream, a financial platform that involved users investing in forex and getting returns based on stipulated timeframe.',
-        points: [
-          'Served as the UX Engineer, intersecting between designs and code implementation',
-          'Developed and maintained code for in-house and client websites primarily using HTML, CSS, JavaScript, and jQuery',
-          'Built a multi-feature audio and video call application powered by React.',
-          'Tested sites in various browsers and devices to ensure cross-browser compatibility and mobile responsiveness.',
-        ],
-      },
-    ],
-    technologies: [
-      'JavaScript (ES6+), TypeScript, Rust, HTML & CSS (Sass) Python GraphQL'
-    ],
-    libraries: [
-        'React, Vue, NodeJs, Tailwind, Express, Solidity'
+  companies: [
+    {
+      name: "@Clinikli",
+      period: "Nov 2021 - August 2022",
+      role: "UI Engineer",
+      term: "Part-Time Role",
+      points: [
+        "I managed and built a user interface that would handle medical appointment booking with doctors, hospitals and clinicians from scratch",
+        "I wrote very performant code that was scalable, highly secure,compatible, and efficient to perform across devices and browsers",
+        "Integrated and harnessed secure and necessary libraries, and also javascript testing in a bid to enable performance and efficiency and increase user experience flow",
       ],
-      design: [
-        'Sketch, Figma'
-      ], 
-
-    others: [
-      'Performance Optimization, End-To-End Testing, Design Systems, Build Automation, Progressive Enhancement,Component Library Build',
-    ],
-  }
+    },
+    {
+      name: "@Sonvisage",
+      period: "August 2021 - March 2022",
+      role: "UI Engineer",
+      term: "Part-Time Role",
+      points: [
+        "Managed and built a user interface for an IOT company, which would also collect data for millions of users",
+        "I wrote modern, clean and manageable code in rebuilding the entire application",
+        "Optimized the use of Vue Js to ensure the application is lightweight, using only necessary libraries for enhancement of the UI performance e.g in the data visualization components",
+        "I was involved in handling continuous integration for the frontend to ensure delivery of high quality code/platform and uncompromised integrity",
+      ],
+    },
+    {
+      name: "@Vinca",
+      period: "June 2021 - August 2021",
+      role: "UI Engineer",
+      term: "Part-Time Role",
+      intro: `Built the entire Ui of the web-app, which would be handling appointment booking and clients management, alongside communication`,
+      points: [
+        "I implemented a simple and minimalistic web app meant to handle hospital appointment booking and management using an automated admin system",
+        "I was involved in writing and implementing highly performant and clean manageable react code",
+        "I handled the implementation of the design from scratch to complete, carried out unit testing to ensure all code components are working effectively and remove excess chunks to achieve lightweight and speed",
+      ],
+    },
+    {
+      name: "@FourthStream",
+      period: "December 2020 - April 2021 (Remote)",
+      role: "Frontend Developer",
+      term: " Contract",
+      points: [
+        "I designed and built a user interface that would handle and manage payment of funds to investors in a financial resource company",
+        "I wrote a robust code for the user interface that will enhance easy interaction for users",
+        "I interacted with the earlier users, to implement requested features, and fully delivered a secured and scalable platform for users alongside the admin system",
+      ],
+    },
+  ],
+  technologies: [
+    "ReactJs, NodeJs, VueJs, Solidity, Rust, JavaScript, HTML & CSS(SCSS), Technical Writing, Open Source, GraphQl, Figma & Sketch",
+  ],
+  others: [
+    "Performance Optimization, End-To-End Testing, Design Systems, Build Automation, Progressive Enhancement",
+  ],
+};
