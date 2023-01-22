cfb116de98ef4955f38638a75ca44d179b5381b2
Ojochogwu <07afrik@gmail.com>
2023-01-23 00:36:10 +0100

    rmi

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,29 +1,76 @@
 <template>
-  <div class=" flex flex-row sx:flex-col h-screen sx:w-11/12   w-10/12 m-auto justify-center items-center mt-36">
-    <div class=" flex flex-col text-left sx:w-full w-2/4 sx:mt-28 m-auto">
-    <div class=" text-5xl font-bold tracking-wide w-2/3 sx:text-3xl text-header">In the last few years;</div>
-      <div class=" text-white text-sm mt-4  leading-7 sx:leading-6 tracking-wide sx:tracking-normal sx:font-normal line">I have been oppurtuned to put my skill to practice by building for different companies and organizations as a Frontend engineer with indebt ability to transform literal designs into functional UI's and scalable applications optimized for best performance.
-         <br class=" mb-6"/>
-        I had the first real-time oppurtunity to build  for a financial capital company -  Fourthstream capital in 2020 on a contract job which lasted a few Months, after which i have been oppurtuned to work in startups & establishments majorly healthcare startups as a UX Developer between 2020 - 2022. 
-        <br class=" mb-6">
-        Haven tried my hands on writing Solidity & Rust, i can relatably rate my expertise even if yet to have a first time contract or Job, i have solved a number of problems blockchain related by trying out my knowldege in the field. 
+  <div
+    class="flex flex-row sx:flex-col h-screen sx:w-11/12 w-10/12 m-auto justify-center items-center mt-36"
+  >
+    <div class="flex flex-col text-left sx:w-full w-2/4 sx:mt-28 m-auto">
+      <div
+        class="text-5xl font-bold tracking-wide w-2/3 sx:text-3xl text-header"
+      >
+        In the last few years;
+      </div>
+      <div
+        class="text-white text-sm mt-4 leading-7 sx:leading-6 tracking-wide sx:tracking-normal sx:font-normal line"
+      >
+        I have been oppurtuned to put my skill to practice by building for
+        different companies and organizations as a Frontend engineer with indebt
+        ability to transform literal designs into functional UI's and scalable
+        applications optimized for best performance.
+        <br class="mb-6" />
+        I had the first real-time oppurtunity to build for a financial capital
+        company - Fourthstream capital in 2020 on a contract job which lasted a
+        few Months, after which i have been oppurtuned to work in startups &
+        establishments majorly healthcare startups as a UX Developer between
+        2020 - 2022.
+        <br class="mb-6" />
+        Haven tried my hands on writing Solidity & Rust, i can relatably rate my
+        expertise even if yet to have a first time contract or Job, i have
+        solved a number of problems blockchain related by trying out my
+        knowldege in the field.
       </div>
     </div>
-    <div class="sx:w-3/4 sx:mt-10 sx:justify-center sx:items-center w-1/2 m-auto flex justify-center items-start">
-        <img class=" w-5/6 sx:w-full relative" src="@/assets/Images/work.svg" alt=""/>
+    <div
+      class="sx:w-full sx:mt-10 w-1/2 m-auto flex flex-col justify-center items-center"
+    >
+      <img class="w-full relative" src="@/assets/Images/sft-2.svg" alt="" />
+      <div class="text-left w-full -mt-36 flex flex-col">
+        <div
+          class="text-xl w-max bg-header bg-opacity-30 p-2 mt-1 text-gray-400 font-bold"
+        >
+          CLINIKLI
+        </div>
+        <div
+          class="text-xl ml-18 w-max bg-header bg-opacity-30 p-2 mt-1 text-gray-400 font-bold"
+        >
+          SONVISAGE
+        </div>
+        <div
+          class="text-xl vn bg-header rotate-90 w-max bg-opacity-30 p-2 mt-1 text-gray-400 font-bold"
+        >
+          VINCA
+        </div>
+        <div
+          class="text-xl mt-4 bg-header w-max bg-opacity-30 p-2 mt-1 text-gray-400 font-bold"
+        >
+          FOURTHSTREAM CAPITAL
+        </div>
       </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  components:{},
-  setup() {
-    
+  components: {},
+  setup() {},
+  data() {
+    return {};
   },
-}
+};
 </script>
 <style scoped>
-.line{
+.line {
   font-size: 13px;
+}
+.vn {
+  transform: rotate(270deg);
 }
 </style>
